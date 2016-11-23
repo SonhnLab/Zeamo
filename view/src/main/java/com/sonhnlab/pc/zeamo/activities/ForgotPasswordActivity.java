c08a6baeaf8b1cbf0c9aa449cc5f243a0810811c
SONHNLAB-PC\SonhnLab <ngocsonitbk@gmail.com>
2016-11-24 01:37:19 +0700

    Handle validate in Forgot Password screen

--- view/src/main/java/com/sonhnlab/pc/zeamo/activities/ForgotPasswordActivity.java
+++ view/src/main/java/com/sonhnlab/pc/zeamo/activities/ForgotPasswordActivity.java
@@ -3,10 +3,15 @@ package com.sonhnlab.pc.zeamo.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sonhnlab.pc.core.BR;
 import com.sonhnlab.pc.core.helper.KeyboardHelper;
@@ -22,6 +27,18 @@ import com.sonhnlab.pc.zeamo.databinding.ActivityForgotPasswordBinding;
 
 public class ForgotPasswordActivity extends BaseActivity<ActivityForgotPasswordBinding, ForgotPasswordViewModel> {
 
+    //region Properties
+
+    private EditText mEmailText;
+
+    private TextView mValidate;
+
+    ImageView mSubmitButton;
+
+    LinearLayout mLayout;
+
+    //endregion
+
     //region Override method
 
     @Override
@@ -29,6 +46,18 @@ public class ForgotPasswordActivity extends BaseActivity<ActivityForgotPasswordB
         App.sharedComponent().inject(this);
         super.onCreate(savedInstanceState);
         setBindingContentView(R.layout.activity_forgot_password, BR.viewModel);
+
+        mEmailText = (EditText) findViewById(R.id.edt_forgot);
+        mValidate = (TextView) findViewById(R.id.tv_forgot_validate);
+        mSubmitButton = (ImageView) findViewById(R.id.btn_forgot_submit);
+        mLayout = (LinearLayout) findViewById(R.id.layout_forgot_edt);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submit();
+            }
+        });
 
         setupToolbar();
         setupParent(findViewById(R.id.layout_forgot_password));
@@ -60,7 +89,7 @@ public class ForgotPasswordActivity extends BaseActivity<ActivityForgotPasswordB
 
     public void setupParent(View view) {
         //Set up touch listener for non-text box views to hide keyboard.
-        if(!(view instanceof EditText)) {
+        if (!(view instanceof EditText)) {
             view.setOnTouchListener(new View.OnTouchListener() {
                 public boolean onTouch(View v, MotionEvent event) {
                     KeyboardHelper.hideSoftKeyboardOnTap(ForgotPasswordActivity.this);
@@ -75,6 +104,31 @@ public class ForgotPasswordActivity extends BaseActivity<ActivityForgotPasswordB
                 setupParent(innerView);
             }
         }
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = mEmailText.getText().toString();
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mValidate.setVisibility(View.VISIBLE);
+            valid = false;
+        }
+        return valid;
+    }
+
+    public void submit() {
+        if (!validate()) {
+            mLayout.setBackground(getDrawable(R.drawable.textfill_a));
+            return;
+        }
+        mLayout.setBackground(getDrawable(R.drawable.textfill));
+        mValidate.setVisibility(View.GONE);
+        finish();
+        Toast.makeText(getApplicationContext(), "Instruction to reset password have been emailed to "
+                + mEmailText.getText().toString()
+                + ". Please check your email for these next steps.", Toast.LENGTH_LONG).show();
     }
 
     //endregion
