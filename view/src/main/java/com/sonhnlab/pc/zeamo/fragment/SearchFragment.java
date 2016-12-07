3bce2e40c3eb2f6724058d0fd3e890c6bb9e354d
SONHNLAB-PC\SonhnLab <ngocsonitbk@gmail.com>
2016-12-07 23:14:51 +0700

    Add performance UI search bar

--- view/src/main/java/com/sonhnlab/pc/zeamo/fragment/SearchFragment.java
+++ view/src/main/java/com/sonhnlab/pc/zeamo/fragment/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.sonhnlab.pc.zeamo.fragment;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +12,13 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.sonhnlab.pc.core.helper.KeyboardHelper;
 import com.sonhnlab.pc.core.view.BaseFragment;
 import com.sonhnlab.pc.core.viewmodel.SearchViewModel;
 import com.sonhnlab.pc.zeamo.App;
@@ -31,6 +34,8 @@ import com.sonhnlab.pc.zeamo.databinding.FragmentSearchBinding;
 public class SearchFragment extends BaseFragment<FragmentSearchBinding,SearchViewModel> {
 
     //region Constructor
+
+    private EditText mSearch;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -65,9 +70,9 @@ public class SearchFragment extends BaseFragment<FragmentSearchBinding,SearchVie
 
         //Setup SearchBar
         final ImageView closeButton = (ImageView) binding.getRoot().findViewById(R.id.im_search_close);
-        final EditText search = (EditText) binding.getRoot().findViewById(R.id.edt_search_bar);
+        mSearch = (EditText) binding.getRoot().findViewById(R.id.edt_search_bar);
 
-        search.addTextChangedListener(new TextWatcher() {
+        mSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -75,8 +80,10 @@ public class SearchFragment extends BaseFragment<FragmentSearchBinding,SearchVie
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.length() != 0) {
+                if (!charSequence.toString().isEmpty()) {
                     closeButton.setVisibility(View.VISIBLE);
+                } else {
+                    closeButton.setVisibility(View.INVISIBLE);
                 }
             }
 
@@ -89,9 +96,9 @@ public class SearchFragment extends BaseFragment<FragmentSearchBinding,SearchVie
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                search.setText("");
-                search.setHint("Search nearby");
-                closeButton.setVisibility(View.GONE);
+                mSearch.setText("");
+                mSearch.setHint("Search nearby");
+                closeButton.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -107,6 +114,9 @@ public class SearchFragment extends BaseFragment<FragmentSearchBinding,SearchVie
         adapter.setViewModel(mViewModel);
         recyclerView.setAdapter(adapter);
 
+        //Setup Parent
+        setupParent(binding.getRoot().findViewById(R.id.layout_search));
+
         setViewDataBinding(binding);
 
         return binding.getRoot();
@@ -114,9 +124,34 @@ public class SearchFragment extends BaseFragment<FragmentSearchBinding,SearchVie
 
     //endregion
 
-    //region Private method
+    //region Public method
 
-
+    public void setupParent(View view) {
+        //Set up touch listener for non-text box views to hide keyboard.
+        if(!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        if (mSearch.isFocused()) {
+                            Rect outRect = new Rect();
+                            if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())){
+                                mSearch.clearFocus();
+                                KeyboardHelper.hidden(getContext(), getActivity().getCurrentFocus());
+                            }
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+        //If a layout container, iterate over children
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupParent(innerView);
+            }
+        }
+    }
 
     //endregion
 }
--- viewmodel/src/main/java/com/sonhnlab/pc/core/helper/KeyboardHelper.java
+++ viewmodel/src/main/java/com/sonhnlab/pc/core/helper/KeyboardHelper.java
@@ -2,6 +2,7 @@ package com.sonhnlab.pc.core.helper;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.IBinder;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -27,9 +28,21 @@ public class KeyboardHelper {
         }
     }
 
+    public static void showSoftKeyboard(Context context, View viewFocus) {
+        if (viewFocus != null) {
+            InputMethodManager manager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.toggleSoftInputFromWindow(viewFocus.getWindowToken(), InputMethodManager.SHOW_FORCED, 0);
+        }
+    }
+
     public static void hideSoftKeyboardOnTap(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public static void hideSoftKeyboard(Context context, IBinder windowToken) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(windowToken,0);
     }
 
     //endregion
